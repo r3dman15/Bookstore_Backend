f71395a2bb9155c5d9ff765d5fa4562be8cd4a61
mwrig067 <mwright067@fiu.edu>
2023-11-03 17:57:52 -0500

    updated seller

--- src/main/java/Domain/Seller.java
+++ src/main/java/Domain/Seller.java
@@ -4,15 +4,11 @@ import java.util.ArrayList;
 
 public class Seller extends User {
     private String storeName;
-    private String location;
-    private int promotionCode;
     private double monthlyPayout;
     private ArrayList<Book> inventory;
-    public Seller(String firstName, String lastName, String id, String email, String storeName, String location) {
+    public Seller(String firstName, String lastName, String id, String email, String storeName) {
         super(firstName, lastName, id, email);
         this.storeName = storeName;
-        this.location = location;
-        this.promotionCode = 0;
         this.monthlyPayout = 0;
         this.inventory = new ArrayList<>();
     }
@@ -20,15 +16,6 @@ public class Seller extends User {
     public String getStoreName() {
         return storeName;
     }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public int getPromotionCode() {
-        return promotionCode;
-    }
-
     public double getMonthlyPayout() {
         return monthlyPayout;
     }
--- src/main/java/Domain/TestSeller.java
+++ src/main/java/Domain/TestSeller.java
@@ -2,13 +2,8 @@ package Domain;
 
 public class TestSeller {
     public static void main(String[] args) {
-        Seller seller1 = new Seller("John", "Doe", "123456", "john@example.com", "Amazon", "New York");
-
-        // Use the constructor to set the seller's information.
-
-        // Get the shop name using a getter method.
+        Seller seller1 = new Seller("John", "Doe", "john@example.com", "Amazon", "New York");
         String shopName = seller1.getShopName();
-
         System.out.println("Seller Shop Name: " + shopName );
     }
 }
